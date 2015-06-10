cddf01ef89bf03ec8ca9f4d9745dc62e68c7c4b5
SARA GUDEMAN <sgudeman982@gmail.com>
2015-06-10 16:20:31 -0700

    (Prep) Added MDN stringify specifications in comments

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,27 @@
 
 // but you don't so you're going to write it from scratch:
 
+// INPUT: collection
+// OUTPUT: JSON string
 var stringifyJSON = function(obj) {
-  // your code goes here
 };
+
+// Description
+// JSON.stringify() converts a value to JSON notation representing it:
+
+// 1. Properties of non-array objects are not guaranteed to be stringified in
+// any particular order. Do not rely on ordering of properties within the same
+// object within the stringification.
+
+// 2. Boolean, Number, and String objects are converted to the corresponding
+// primitive values during stringification, in accord with the traditional
+// conversion semantics.
+
+// 3. If undefined, a function, or a symbol is encountered during conversion it
+// is either omitted (when it is found in an object) or censored to null
+// (when it is found in an array).
+
+// 4. All symbol-keyed properties will be completely ignored, even when using
+// the replacer function.
+
+// 5. Non-enumerable properties will be ignored
