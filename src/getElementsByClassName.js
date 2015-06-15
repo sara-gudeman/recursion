46f200648cd42d987fb1a59e49688186858a5036
SARA GUDEMAN <sgudeman982@gmail.com>
2015-06-14 19:40:07 -0700

    (Feat) Inner fcn filters for nodes w/className

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -9,15 +9,19 @@
 // You should use document.body, element.childNodes, and element.classList
 var getElementsByClassName = function(className){
   var hasClass = [];
-  // inner fcn takes in class name & DOM nodes to be searched
+  // inner fcn takes in class name & DOM nodes (array) to be searched
   var filterElements = function(className, elements) {
+    // filter elements that contain className
+    var filteredClass = elements.filter(function(element) {
+      if (element.classList !== undefined) {
+        return element.classList.contains(className);
+      }
+    });
+    // store new elements w/className
+    hasClass = hasClass.concat(filteredClass);
   };
-  // process:
-  // search parent container (initially document)
-  // iterate over nodes in parent
-  // --if node contains class name: push to array
-  // --if node contains children, search child nodes for class (recursive step)
-  // --if no more nodes: return array
-  var body = document.body;
-  return filterElements(className, body); 
+
+  var body = document.body.childNodes;
+  var bodyArr = Array.prototype.slice.call(body);
+  return filterElements(className, bodyArr); 
 };
