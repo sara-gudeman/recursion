3cfc5fc3da7d8fa53af4b1b9d24f8fd44a563eae
SARA GUDEMAN <sgudeman982@gmail.com>
2015-06-14 19:12:21 -0700

    (Prep)Starting over. Copying nodes to arrays by using slice

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,32 +8,13 @@
 // from bookstrap:
 // You should use document.body, element.childNodes, and element.classList
 var getElementsByClassName = function(className){
-  var nodeArr = [];
-  var searchParent = function(className, parent) {
-    var children = parent.childNodes;
-    for (var i = 0; i < children.length; i++) {
-      // console.log(children[i]);
-      if (children[i].classList !== undefined) {
-        // console.log(children[i].classList);
-        // console.log(children[i].classList.contains(className));
-      }
-      // console.log(children[i].childNodes.length > 0);
-      if (children[i].childNodes.length > 0) {
-        console.log("recursed");
-        return searchParent(className, children[i]);
-      }
-    }
-  }
-  searchParent(className, document.body);
-  return nodeArr;
+  // Goal: return all elements containing class name
+  // input: class name
+  // output: array of children containing class name
+  // process:
+  // search parent container (initially document)
+  // iterate over nodes in parent
+  // --if node contains class name: push to array
+  // --if node contains children, search child nodes for class (recursive step)
+  // --if no more nodes: return array
 };
-
-// Goal: return all elements containing class name
-// input: class name
-// output: array of children containing class name
-// process:
-// search parent container (initially document)
-// iterate over nodes in parent
-// --if node contains class name: push to array
-// --if node contains children, search child nodes for class (recursive step)
-// --if no more nodes: return array
